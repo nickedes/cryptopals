92e1cee00fad770e28ed4dea2bbf4d33201ee16b
nikhil <nkmittal4994@gmail.com>
2018-01-17 00:45:27 +0530

    solved challenge 1

--- Set 1/challenge1.py
+++ Set 1/challenge1.py
@@ -1,7 +1,13 @@
 # http://cryptopals.com/sets/1/challenges/1
+import base64 as b
 
 # given Hex!
 string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-# Convert it to Base 64!
+# Decodes from hex to bytes object
+encoded = bytes.fromhex(string)
 
+# Encode the bytes object using Base64
+b64encode = b.b64encode(encoded)
+
+print(b64encode)
