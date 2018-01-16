57da79648e09417d569a921c38be8cae7be4ace3
nikhil <nkmittal4994@gmail.com>
2018-01-16 22:06:11 +0530

    challenge 1

---
+++ Set 1/challenge1.py
@@ -0,0 +1,7 @@
+# http://cryptopals.com/sets/1/challenges/1
+
+# given Hex!
+string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+# Convert it to Base 64!
+
