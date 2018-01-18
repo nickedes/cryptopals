f66fd55f76385a7bd20d8a00adfebdc4de986735
nikhil <nkmittal4994@gmail.com>
2018-01-18 11:06:48 +0530

    XOR-ing for multiple with 256 possibilities!:

--- Set 1/challenge4.py
+++ Set 1/challenge4.py
@@ -1,2 +1,23 @@
+from challenge3 import getScore
+import time
+
 with open('challenge-data-4.txt', 'r') as f:
-    print(f.read(), "here")
+    data = f.read()
+
+lines = data.split('\n')
+
+strings = []
+for line in lines:
+    dec_str = bytes.fromhex(line)
+    for i in range(256):
+        plain = ''
+        for x in dec_str:
+            plain += chr(x ^ i)
+        score = getScore(plain)
+        strings.append((plain, score))
+
+sorted_byscore = sorted(strings, key=lambda x: x[1], reverse=True)
+
+# print(sorted_byscore[:5])
+for x in sorted_byscore[:5]:
+    print(x[0], "Score : ", x[1])
