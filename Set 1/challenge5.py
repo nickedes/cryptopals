ce18afe82970b97222cd4c203bf8636a1427b946
nikhil <nkmittal4994@gmail.com>
2018-01-19 00:13:26 +0530

    repeating key xor-ing!

--- Set 1/challenge5.py
+++ Set 1/challenge5.py
@@ -1,3 +1,17 @@
-plaintext = "Burning 'em, if you ain't quick and nimble\
-I go crazy when I hear a cymbal"
+plaintext = msg = """Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
 
+key = "ICE"
+
+expected_hex = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+ciphertext = ""
+
+for i in range(len(plaintext)):
+    ciphertext += chr(ord(plaintext[i]) ^ ord(key[i % 3]))
+
+encoded = ciphertext.encode()
+
+hexcode = encoded.hex()
+
+print(hexcode == expected_hex)
