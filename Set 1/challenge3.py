90cf4589ac4d23a2e15182a0d9ce6f0d963cf64c
nikhil <nkmittal4994@gmail.com>
2018-01-18 10:25:34 +0530

    frequency checks and XOR-ing!

---
+++ Set 1/challenge3.py
@@ -0,0 +1,61 @@
+# http://cryptopals.com/sets/1/challenges/3
+
+# hex encoded string
+hexencoded = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+freqtable = {"E": "12.02",
+             "T": "9.10",
+             "A": "8.12",
+             "O": "7.68",
+             "I": "7.31",
+             "N": "6.95",
+             "S": "6.28",
+             "R": "6.02",
+             "H": "5.92",
+             "D": "4.32",
+             "L": "3.98",
+             "U": "2.88",
+             "C": "2.71",
+             "M": "2.61",
+             "F": "2.30",
+             "Y": "2.11",
+             "W": "2.09",
+             "G": "2.03",
+             "P": "1.82",
+             "B": "1.49",
+             "V": "1.11",
+             "K": "0.69",
+             "X": "0.17",
+             "Q": "0.11",
+             "J": "0.10",
+             "Z": "0.07"
+             }
+
+
+def getScore(plain, freqtable):
+    score = 0
+    for char in plain:
+        if not char.isalpha():
+            # penalty
+            score -= 15
+        elif char.isalpha() and char.upper() in freqtable:
+            score += float(freqtable[char.upper()])
+    return score
+
+dec_str = bytes.fromhex(hexencoded)
+
+strings = []
+for i in range(97, 97+26):
+    plain = ''
+    for x in dec_str:
+        plain += chr(x ^ i)
+    score = getScore(plain, freqtable)
+    strings.append((plain, score))
+
+# cOOKINGmcSLIKEAPOUNDOFBACON
+sorted_byscore = sorted(strings, key=lambda x: x[1], reverse=True)
+
+print(sorted_byscore)
+
+# 1st string!
+print(sorted_byscore[0][0])
