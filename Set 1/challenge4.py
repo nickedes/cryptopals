941c0d6d781e15c65baa0036822a46d09ef12ba2
nikhil <nkmittal4994@gmail.com>
2018-01-20 00:20:31 +0530

    added functions for single xor detect

--- Set 1/challenge4.py
+++ Set 1/challenge4.py
@@ -1,23 +1,28 @@
 from challenge3 import getScore
-import time
 
-with open('challenge-data-4.txt', 'r') as f:
-    data = f.read()
 
-lines = data.split('\n')
+def single_char_xor(lines):
+    strings = []
+    for line in lines:
+        dec_str = bytes.fromhex(line)
+        for i in range(256):
+            plain = ''
+            for x in dec_str:
+                plain += chr(x ^ i)
+            score = getScore(plain)
+            strings.append((plain, score))
 
-strings = []
-for line in lines:
-    dec_str = bytes.fromhex(line)
-    for i in range(256):
-        plain = ''
-        for x in dec_str:
-            plain += chr(x ^ i)
-        score = getScore(plain)
-        strings.append((plain, score))
+    sorted_byscore = sorted(strings, key=lambda x: x[1], reverse=True)
+    return sorted_byscore
 
-sorted_byscore = sorted(strings, key=lambda x: x[1], reverse=True)
 
-# print(sorted_byscore[:5])
-for x in sorted_byscore[:5]:
-    print(x[0], "Score : ", x[1])
+if __name__ == '__main__':
+    with open('challenge-data-4.txt', 'r') as f:
+        data = f.read()
+
+    lines = data.split('\n')
+    sorted_byscore = single_char_xor(lines)
+
+    # print(sorted_byscore[:5])
+    for x in sorted_byscore[:5]:
+        print(x[0], "Score : ", x[1])
