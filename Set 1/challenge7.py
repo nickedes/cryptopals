2db403d4c2721d655d30207845da23aa860ec400
nikhil <nkmittal4994@gmail.com>
2018-01-21 22:05:09 +0530

    Aes! with Ecb

--- Set 1/challenge7.py
+++ Set 1/challenge7.py
@@ -1,8 +1,19 @@
 import base64 as b
-
+# Using this! https://github.com/dlitz/pycrypto
+from Crypto.Cipher import AES
 
 if __name__ == '__main__':
     with open('challenge-data-7.txt', 'r') as f:
         data = f.read()
-    b64decode = b.b64decode(data)
-    print(b64decode)
+
+    ciphertext = b.b64decode(data)
+    key = 'YELLOW SUBMARINE'
+
+    # Default mode for AES in package is ECB
+    cipher = AES.new(key)
+
+    encodedText = cipher.decrypt(ciphertext)
+    # plaintext = encodedText.decode()
+    # Decode the bytes to String!
+    plaintext = str(encodedText, 'utf-8')
+    print(plaintext)
